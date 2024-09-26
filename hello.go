cead7fbd3985752c7c89004fb5e555463ddb976a
Allensree Binoj Kummen <allen@hamon.in>
2024-09-26 17:08:43 +0530

    Adds first exercise in go

--- hello.go
+++ hello.go
@@ -1,1 +1,15 @@
+package main
 
+import (
+	"fmt" // A package in the Go standard library.
+	// "io"               // Implements some I/O utility functions.
+	// m "math"           // Math library with local alias m.
+	// "net/http"         // Yes, a web server!
+	// _ "net/http/pprof" // Profiling library imported only for side effects
+	// "os"               // OS functions like working with the file system
+	// "strconv"          // String conversions.
+)
+
+func main() {
+	fmt.Println("Hello GO")
+}
